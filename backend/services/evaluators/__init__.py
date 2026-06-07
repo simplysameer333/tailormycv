4706029fb1da7d2215d43c3b64471bd01a314e10
Sameer Hameed <samorsameer@gmail.com>
2026-06-07 10:46:54 +0100

    chore: remove dead services/evaluators/ (replaced by pipeline agents)

--- backend/services/evaluators/__init__.py
+++
@@ -1,7 +0,0 @@
-from .claude_evaluator import AnthropicEvaluator
-from .gpt4_evaluator import OpenAIEvaluator
-from .gemini_evaluator import GoogleEvaluator
-
-EVALUATOR_REGISTRY = [AnthropicEvaluator(), OpenAIEvaluator(), GoogleEvaluator()]
-
-__all__ = ["AnthropicEvaluator", "OpenAIEvaluator", "GoogleEvaluator", "EVALUATOR_REGISTRY"]
